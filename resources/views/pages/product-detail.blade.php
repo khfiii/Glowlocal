205f1b6e60a9ee4898a75a0ee6b4102a08bbb5b0
khfiii <khfii635@gmail.com>
2024-11-05 22:00:24 +0700

    Add breadcrumbs

--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -1,6 +1,47 @@
 <x-app-layout>
     @section('page-title', $product->title)
 
+
+
+    <nav class="flex ms-4" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li class="inline-flex items-center gap-2">
+                <a href="{{ route('home') }}"
+                    class="inline-flex items-center text-sm font-medium hover:text-gray-600 text-gray-500">
+                    <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                        viewBox="0 0 20 20">
+                        <path
+                            d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                    </svg>
+                    Home
+                </a>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <a href="{{ route('product') }}"
+                        class="ms-1 text-sm font-medium hover:text-gray-600 md:ms-2 text-gray-500">Products</a>
+                </div>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <a href="#"
+                        class="ms-1 text-sm font-medium hover:text-gray-600 md:ms-2 text-gray-500">{{ $product->category->name }}</a>
+                </div>
+            </li>
+        </ol>
+    </nav>
+
+
     <div class="font-sans">
         <div class="p-4 lg:max-w-7xl max-w-xl max-lg:mx-auto">
             <div class="grid items-start grid-cols-1 lg:grid-cols-5 gap-12">
