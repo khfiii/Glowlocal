bb476dc7baa07d8155f743143630867a7ba09357
khfiii <khfii635@gmail.com>
2024-11-08 14:42:49 +0700

    Update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
 
 <!-- component -->
 <!-- This is an example component -->
-<div class="h-screen w-screen bg-gray-100 flex items-center">
+<div class="h-[40rem] w-screen bg-gray-100 flex items-center">
 	<div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
    		<div class="max-w-md">
       		<div class="text-5xl font-dark font-bold">404</div>
