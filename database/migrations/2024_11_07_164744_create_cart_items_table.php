da8ecdefd6aae33d583a5b25945601ba23fdedb3
khfiii <khfii635@gmail.com>
2024-11-08 14:18:37 +0700

    Add cart feature

---
+++ database/migrations/2024_11_07_164744_create_cart_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Cart::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -25,30 +25,31 @@
                         {{ __('Ketentuan Layanan') }}
                     </x-nav-link>
                     @guest
-                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')" wire:navigate>
-                            {{ __('Login') }}
-                        </x-nav-link>
+                    <x-nav-link :href="route('login')" :active="request()->routeIs('login')" wire:navigate>
+                        {{ __('Login') }}
+                    </x-nav-link>
                     @endguest
 
                     @auth()
-                        <x-nav-link :href="route('logout.get')" :active="request()->routeIs('login')" wire:navigate>
-                            {{ __('Logout') }}
-                        </x-nav-link>
+                    <x-nav-link :href="route('logout.get')" :active="request()->routeIs('login')" wire:navigate>
+                        {{ __('Logout') }}
+                    </x-nav-link>
                     @endauth
+                    <x-nav-link :href="route('logout.get')" :active="request()->routeIs('login')" wire:navigate>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </x-nav-link>
                 </div>
             </div>
 
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -73,18 +74,29 @@
                 {{ __('Ketentuan Layanan') }}
             </x-responsive-nav-link>
             @guest
-                <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')" wire:navigate>
-                    {{ __('Login') }}
-                </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')" wire:navigate>
+                {{ __('Login') }}
+            </x-responsive-nav-link>
             @endguest
 
             @auth()
-                 <x-responsive-nav-link :href="route('logout.get')" :active="request()->routeIs('auth/logout')" wire:navigate>
-                            {{ __('Logout') }}
-                </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('logout.get')" :active="request()->routeIs('auth/logout')" wire:navigate>
+                {{ __('Logout') }}
+            </x-responsive-nav-link>
             @endauth
+            <x-responsive-nav-link :href="route('logout.get')" :active="request()->routeIs('auth/logout')" wire:navigate>
+
+                <div class="flex gap-2 items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                  </svg>
+              
+                </div>
+
+            </x-responsive-nav-link>
         </div>
     </div>
 
 
 </nav>
+
