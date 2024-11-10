5056a3132c392caab1ee480e436c062255e30001
khfiii <khfii635@gmail.com>
2024-11-10 09:30:27 +0700

    update

--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -7,6 +7,9 @@
                 <div x-cloack
                     class="min-h-[250px] md:min-h-[500px] lg:col-span-3 bg-gray-50 rounded-lg w-full lg:sticky top-0 text-center">
 
+                        @dump(( config('services.google.client_id') ))
+                        @dump(( config('services.google.client_secret') ))
+                    
                     <div id="main-carousel" class="splide">
                         <div class="splide__track">
                             <ul class="splide__list">
