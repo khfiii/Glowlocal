8d121aa7ec4137243c1a07d9504bb6c517cd30a4
khfiii <khfii635@gmail.com>
2024-11-17 19:13:40 +0700

    update

--- app/Http/Controllers/ShoppingController.php
+++ app/Http/Controllers/ShoppingController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ShoppingController extends Controller {
     public function index() {
         $orders = Order::where( 'user_id', auth()->user()->id )
-        ->with( 'items.product.media' )->get();
+        ->with( 'items.product.media' )->latest()->get();
 
         return view( 'pages.history', compact( 'orders' ) );
     }
