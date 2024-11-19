86242d5a7f7fd84e5b03e2fb3382f6b281bc4baf
khfiii <khfii635@gmail.com>
2024-11-19 15:53:08 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Schedule::command( 'queue:work --stop-when-empty' )
+// ->everyMinute();
+
 Schedule::command( 'queue:work --stop-when-empty' )
-->everyMinute();
+->everyMinute()
+->withoutOverlapping();
 
