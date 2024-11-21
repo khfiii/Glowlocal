e4cf52e8738a74144a1019b9db3fd96a11fcfc60
khfiii <khfii635@gmail.com>
2024-11-21 20:46:24 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('page-title') - {{ config('app.name', 'Glowlocal Shop') }} 
+    <title>@yield('page-title') - {{ config('app.name', 'Glowlocal Shop') }}
     </title>
+
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org"
+            , "@type": "WebSite"
+            , "name": "Glowlocal Shop"
+            , "alternateName": "Glowlocal"
+            , "url": "https://glowlocal.shop/"
+            , "potentialAction": {
+                "@type": "SearchAction"
+                , "target": {
+                    "@type": "EntryPoint"
+                    , "urlTemplate": "https://glowlocal.shop/search?q={search_term_string}"
+                }
+                , "query-input": "required name=search_term_string"
+            }
+        }
+
+    </script>
 
 
     <!-- Fonts -->
@@ -19,6 +38,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+
+
 
     <meta name="robots" content="index, follow">
     @stack('meta')
