61ccb2eb243ea6bf8e768af92e1847843ec9258b
khfiii <khfii635@gmail.com>
2024-11-21 20:12:07 +0700

    update

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Article;
-use Spatie\Sitemap\Tags\Url;
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
 
@@ -31,14 +29,7 @@ class GenerateSitemap extends Command {
             $path = public_path( 'sitemap.xml' );
             $url = config( 'app.url' );
             // Menjalankan perintah SitemapGenerator
-            SitemapGenerator::create( 'http://127.0.0.1:8000' )
-            ->getSitemap()
-            ->add(...Article::all()->map(function ($article) {
-                return Url::create(route('detail.article', $article->slug)) // atau slug
-                    ->setLastModificationDate($article->updated_at)
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
-                    ->setPriority(0.8);
-            })->toArray()) // Konversi koleksi ke ar
+            SitemapGenerator::create( $url )
             ->writeToFile( $path );
 
             $this->info( 'Sitemap berhasil dibuat di ' . $path );
