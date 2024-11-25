e355ecb24693281c29eb1a77e30253032b3602ab
khfiii <khfii635@gmail.com>
2024-11-26 02:09:12 +0700

    update

--- app/Providers/MidtransServiceProvider.php
+++ app/Providers/MidtransServiceProvider.php
@@ -18,22 +18,22 @@ class MidtransServiceProvider extends ServiceProvider {
     */
 
     public function boot(): void {
-        // \Midtrans\Config::$serverKey = config( 'midtrans.production.server_key' );
-
-        // // Set to Development/Sandbox Environment ( default ). Set to true for Production Environment ( accept real transaction ).
-        // \Midtrans\Config::$isProduction = config( 'midtrans.production.is_production' );
-        // // Set sanitization on ( default )
-        // \Midtrans\Config::$isSanitized = true;
-        // // Set 3DS transaction for credit card to true
-        // \Midtrans\Config::$is3ds = true;
-
-        \Midtrans\Config::$serverKey = config( 'midtrans.stagging.server_key' );
+        \Midtrans\Config::$serverKey = config( 'midtrans.production.server_key' );
 
         // Set to Development/Sandbox Environment ( default ). Set to true for Production Environment ( accept real transaction ).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = config( 'midtrans.production.is_production' );
         // Set sanitization on ( default )
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
         \Midtrans\Config::$is3ds = true;
+
+        // \Midtrans\Config::$serverKey = config( 'midtrans.stagging.server_key' );
+
+        // // Set to Development/Sandbox Environment ( default ). Set to true for Production Environment ( accept real transaction ).
+        // \Midtrans\Config::$isProduction = false;
+        // // Set sanitization on ( default )
+        // \Midtrans\Config::$isSanitized = true;
+        // // Set 3DS transaction for credit card to true
+        // \Midtrans\Config::$is3ds = true;
     }
 }
