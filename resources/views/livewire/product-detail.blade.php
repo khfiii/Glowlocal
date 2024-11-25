105e32d19b82e71e9677ea53df15fea9b7c7fd42
khfiii <khfii635@gmail.com>
2024-11-25 18:02:25 +0700

    update

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -54,7 +54,7 @@
       <div class="mt-8 space-y-1">
           <h3 class="text-xl font-bold text-gray-800">Deskripsi Produk</h3>
 
-          <div class="text-sm prose prose-sm -space-y-2 text-justify">
+          <div class="text-sm prose prose-sm -space-y-2 text-start">
               {!! Str::markdown($product->description) !!}
           </div>
       </div>
