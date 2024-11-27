7a68fd936db5fb8382330fd0362afc0d03aaae29
khfiii <khfii635@gmail.com>
2024-11-27 18:30:42 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,12 @@ import "../../vendor/masmerise/livewire-toaster/resources/js";
 import "./payment";
 
 Alpine.plugin(intersect);
+
+const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+const isEmbeddedBrowser = /Instagram|TikTok|FBAN|FBAV/.test(userAgent);
+
+console.log(isEmbeddedBrowser);
+
+if (isEmbeddedBrowser) {
+    window.open("https://glowlocal.shop", "_blank"); // URL yang ingin dibuka
+}
