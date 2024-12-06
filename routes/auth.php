b35bbb2d568c89506e44e9e2d415a168a6c2317e
khfiii <khfii635@gmail.com>
2024-12-06 21:35:46 +0700

    udpate

--- routes/auth.php
+++ routes/auth.php
@@ -50,62 +50,20 @@ Route::get('/auth/redirect', function () {
     return Socialite::driver('google')->redirect();
 })->name('google-login');
 
-// Route::get('/auth/google/callback', function () {
-//     $userGoogle = Socialite::driver('google')->stateless()->user();
+Route::get('/auth/google/callback', function () {
+    $userGoogle = Socialite::driver('google')->stateless()->user();
 
-//     $user = User::updateOrCreate(
-//         ['email' => $userGoogle->getEmail()],  // Kondisi pencarian berdasarkan email
-//         [
-//             'name' => $userGoogle->getName(),
-//             'email_verified_at' => Carbon::now(),   // Tandai sebagai email terverifikasi
-//         ]
-//     );
+    $user = User::updateOrCreate(
+        ['email' => $userGoogle->getEmail()],  // Kondisi pencarian berdasarkan email
+        [
+            'name' => $userGoogle->getName(),
+            'email_verified_at' => Carbon::now(),   // Tandai sebagai email terverifikasi
+        ]
+    );
 
-//     Auth::login($user);
+    Auth::login($user);
     
-//     return session()->has('url.intended') ? redirect(session('url.intended')) : redirect()->route('product');
-// });
-
-Route::get('/auth/google/callback', function (Request $request) {
-    try {
-        // Ambil credential dari URL query parameter
-        $idToken = $request->credential;
-
-        Log::info(['data' => $request->all()]);
-
-        if (!$idToken) {
-            return response()->json(['success' => false, 'error' => 'Credential not provided']);
-        }
-
-        // Validasi token dengan Google API
-        $validationUrl = 'https://oauth2.googleapis.com/tokeninfo';
-        $response = Http::get($validationUrl, ['id_token' => $idToken]);
-
-        if ($response->failed()) {
-            return response()->json([
-                'success' => false,
-                'error' => 'Invalid ID token'
-            ]);
-        }
-
-        $googleData = $response->json();
-
-        Log::info(['data' => $googleData]);
-
-        // Simpan atau perbarui pengguna
-        $user = User::updateOrCreate(
-            ['email' => $googleData['email']], // Pencarian berdasarkan email
-            [
-                'name' => $googleData['name'] ?? $googleData['email'],
-                'email_verified_at' => now(),
-            ]
-        );
-
-        Auth::login($user);
-    
-
-        return response()->json(['success' => true]);
-    } catch (\Exception $e) {
-        return response()->json(['success' => false, 'error' => $e->getMessage()]);
-    }
+    return session()->has('url.intended') ? redirect(session('url.intended')) : redirect()->route('product');
 });
+
+
