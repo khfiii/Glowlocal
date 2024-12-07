0074c24c89585b2136954c50f15f33c4136aa58f
khfiii <khfii635@gmail.com>
2024-12-07 17:28:30 +0700

    update

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -54,7 +54,7 @@
 
       @if ($product->description)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
           <div class="text-sm prose prose-sm -space-y-2 text-start">
-          <h2>Deksripsi</h1>
+          <h2>Deskripsi Produk</h1>
               {!! Str::markdown($product->description) !!}
           </div>
       @endif
