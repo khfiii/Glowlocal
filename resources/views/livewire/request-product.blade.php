bada52dac7595ae557703187f1955bc041a6f6d6
khfiii <khfii635@gmail.com>
2024-12-09 00:45:48 +0700

    update

--- resources/views/livewire/request-product.blade.php
+++ resources/views/livewire/request-product.blade.php
@@ -32,7 +32,7 @@
 
                  <input
                      class="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none  disabled:cursor-not-allowed disabled:opacity-50 text-[#030712]"
-                     placeholder="Link Here" wire:model="message">
+                     placeholder="Masukan Link" wire:model="message">
 
 
                  <button
