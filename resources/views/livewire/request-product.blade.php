8ab7335b08dc802a65829954ca86a3346526441d
khfiii <khfii635@gmail.com>
2024-12-09 18:42:24 +0700

    update

--- resources/views/livewire/request-product.blade.php
+++ resources/views/livewire/request-product.blade.php
@@ -1,7 +1,7 @@
  <div x-data="{ active: $wire.entangle('active') }" class="px-10">
      <!-- component -->
      <button @click="active = ! active"
-         class="fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900"
+         class="fixed bottom-20 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900"
          type="button" aria-haspopup="dialog" aria-expanded="false" data-state="closed">
          <svg xmlns=" http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 0 24 24" fill="none"
              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
