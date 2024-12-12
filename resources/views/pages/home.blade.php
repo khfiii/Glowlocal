be89e19640acfa248c4d4c669b06c838307da046
khfiii <khfii635@gmail.com>
2024-12-12 21:04:21 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,10 +5,13 @@
     @push('meta')
         <meta name="description"
             content="Temukan pilihan produk digital berkualitas tinggi dengan harga terjangkau. Glowlocal hadir untuk memenuhi kebutuhanmu akan produk terbaik">
-        <meta name="keywords" content="glowlocal, produk digital, ebook terjemahan indonesia, ebook, twointomedia, ebook gratis bahasa indonesia">
+        <meta name="keywords"
+            content="glowlocal, produk digital, ebook terjemahan indonesia, ebook, twointomedia, ebook gratis bahasa indonesia">
         <meta property="og:title" content="Glowlocal Shop">
         <meta property="og:description"
-            content="Temukan pilihan produk digital berkualitas tinggi nan aesthetic dengan harga terjangkau. Glowlocal hadir untuk memenuhi kebutuhanmu akan produk digital terbaik">
+            content="Jelajahi Beragam Koleksi Ebook Keren, dan Produk Digital 
+                    lainnya
+                    Khusus Untukmu ">
     @endpush
 
 
@@ -20,35 +23,38 @@
         </div>
         <div class="container flex justify-center">
             <div class="text-start md:text-center md:max-w-3xl">
-                <h1 class="title-font sm:text-4xl text-xl mb-4 font-bold text-gray-900 font-serif text-start md:text-center">Gramedia Digital Ditangamu, Baca Dimana dan Kapan Aja</span>
+                <h1
+                    class="title-font sm:text-4xl text-xl mb-4 font-bold text-gray-900 font-serif text-start md:text-center">
+                    Gramedia Digital Ditangamu, Baca Dimana dan Kapan Aja</span>
                 </h1>
                 <p class="mb-8 leading-relaxed font-serif text-start md:text-center">
-                    Jelajahi Beragam Koleksi Ebook Keren, dan Produk Digital 
+                    Jelajahi Beragam Koleksi Ebook Keren, dan Produk Digital
                     lainnya
                     Khusus Untukmu ✨
                 </p>
                 <div class="flex flex-col md:flex-row gap-2 md:justify-center">
 
-                    @if(\Browser::isInApp())
-                        <a href="#" class="relative inline-block px-4 py-2 font-medium group" id="openInBrowserButton">
-                        <span
-                            class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                        <span
-                            class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                        <div class="w-full relative text-center">
-                            <span class=" text-black group-hover:text-white font-serif">Jelajahi Produk</span>
-                        </div>
-                    </a>
+                    @if (\Browser::isInApp())
+                        <a href="#" class="relative inline-block px-4 py-2 font-medium group"
+                            id="openInBrowserButton">
+                            <span
+                                class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                            <span
+                                class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                            <div class="w-full relative text-center">
+                                <span class=" text-black group-hover:text-white font-serif">Jelajahi Produk</span>
+                            </div>
+                        </a>
                     @else
-                    <a href="{{ route('product') }}" class="relative inline-block px-4 py-2 font-medium group">
-                        <span
-                            class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                        <span
-                            class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-                        <div class="w-full relative text-center">
-                            <span class=" text-black group-hover:text-white font-serif">Jelajahi Produk</span>
-                        </div>
-                    </a>     
+                        <a href="{{ route('product') }}" class="relative inline-block px-4 py-2 font-medium group">
+                            <span
+                                class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                            <span
+                                class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                            <div class="w-full relative text-center">
+                                <span class=" text-black group-hover:text-white font-serif">Jelajahi Produk</span>
+                            </div>
+                        </a>
                     @endif
 
                 </div>
