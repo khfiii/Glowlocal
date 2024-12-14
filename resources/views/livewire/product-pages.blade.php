f31b6c7bedec11db1bf59b979894a594c68a66d8
khfiii <khfii635@gmail.com>
2024-12-14 19:53:54 +0700

    update

--- resources/views/livewire/product-pages.blade.php
+++ resources/views/livewire/product-pages.blade.php
@@ -9,7 +9,7 @@
                 <div class="relative">
                     <input
                         class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                        placeholder="Masukan Judul Ebook"  wire:model.blur="search"  />
+                        placeholder="Masukan Judul Ebook" wire:model.blur="search" />
                     <button
                         class="absolute top-1 right-1 flex items-center rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button">
@@ -55,25 +55,29 @@
                         </div>
                     </div>
                 @empty
-                    <p wire:loading.remove class="font-poppins w-full text-center text-sm text-gray-600">Produk belum ada, silahkan request pada icon pesan dibawah</p>
+                    <p wire:loading.remove class="font-poppins w-full text-center text-sm text-gray-600">Produk belum
+                        ada, silahkan request pada icon pesan dibawah</p>
                 @endforelse
 
-                <div class="col-span-full flex justify-start" wire:loading>
-                    <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-                        <div class="animate-pulse flex space-x-4">
-                            <div class="flex-1 space-y-6 py-1">
-                                <div class="h-2 bg-slate-700 rounded"></div>
-                                <div class="space-y-3">
-                                    <div class="grid grid-cols-3 gap-4">
-                                        <div class="h-2 bg-slate-700 rounded col-span-2"></div>
-                                        <div class="h-2 bg-slate-700 rounded col-span-1"></div>
-                                    </div>
+                @for ($i = 0; $i < 3; $i++)
+                    <div class="md:col-span-1 col-span-full flex justify-start" wire:loading>
+                        <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                            <div class="animate-pulse flex space-x-4">
+                                <div class="flex-1 space-y-6 py-1">
                                     <div class="h-2 bg-slate-700 rounded"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                            <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+                                            <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+                                        </div>
+                                        <div class="h-2 bg-slate-700 rounded"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endfor
+
 
                 <div class="col-span-full" wire:loading.remove>
                     {{ $this->products->links() }}
